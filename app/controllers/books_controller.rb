633c12eaed74ac84b62038c8617e6075057bf0d3
EC2 Default User <ec2-user@ip-172-31-17-136.ec2.internal>
2021-02-20 11:43:45 +0000

    made top-page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,29 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/show'
   end
 
   def show
   end
 
   def edit
+  end
+
+
+  private
+
+  def book_params
+    params.permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root to: 'books#top'
-  get 'books/top'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
